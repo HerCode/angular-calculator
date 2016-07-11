7cef24e0236e0df3c24e94da2247b042229eeb67
Sahasha Reese <codeHer@icloud.com>
2016-07-11 05:39:32 -0500

    Initial Commit

---
+++ Shared/calculator.js
@@ -0,0 +1,23 @@
+var box = document.getElementById('#display');
+function addtoscreen(x){
+  box.value = x;
+}
+
+/*box.value = x;*/
+/*angular.module('CalculatorApp', [])
+    .controller('CalculatorController', function($scope) {
+        $scope.result = function() {
+            if ($scope.operator == '+') {
+                return $scope.a + $scope.b;
+            }
+            if ($scope.operator == '-') {
+                return $scope.a - $scope.b;
+            }
+            if ($scope.operator == '*') {
+                return $scope.a * $scope.b;
+            }
+            if ($scope.operator == '/') {
+                return $scope.a / $scope.b;
+            }
+        };
+    });*/
